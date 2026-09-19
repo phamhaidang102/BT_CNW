d9e71187e91807b23d134e191491f0295e7fd0fe
Cansomeonematchmyfreak <phamhaidang123m@gmail.com>
2026-09-19 22:49:12 +0700

    Tuan 4

---
+++ fix_bootstrap.py
@@ -0,0 +1,24 @@
+﻿import os
+import re
+
+def fix_html(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read()
+    except UnicodeDecodeError:
+        with open(filepath, 'r', encoding='latin-1') as f:
+            content = f.read()
+
+    # Fix the double <div class="row"> issue
+    content = content.replace('<div class="row">\n<div class="row">\n<div id="links" class="col-md-3">', '<div class="row">\n<div id="links" class="col-md-3">')
+    
+    # write back
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+base_dir = r"d:\Code\2023_BTHTCNWeb_\Tuan4"
+for root, dirs, files in os.walk(base_dir):
+    for file in files:
+        if file.endswith('.html') or file.endswith('.htmll'):
+            filepath = os.path.join(root, file)
+            fix_html(filepath)
